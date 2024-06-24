135bfd9137d1e62385aefae547a271cf74398b7b
Rolando Abarza <rolo.abarza99@gmail.com>
2024-06-24 18:22:50 -0400

    refactor typing

--- RLib/environments/ssp.py
+++ RLib/environments/ssp.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Dict, Any
 import random
 import numpy as np
 import networkx as nx
@@ -6,86 +7,104 @@ import copy
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from RLib.distributions.distributions import (
-    LogNormalDistribution,
     expected_time,
     random_time,
 )
+
+from RLib.utils.table_utils import dict_states_actions_zeros, dict_states_zeros
 
 ##############################################################################################################
 # Stochastic Shortest Path
 ##############################################################################################################
 
 
-def get_edge_attribute(G, orig, dest, weight="length"):
-    edge_data = G.get_edge_data(orig, dest)
+def get_edge_attribute(G: nx.Graph, source: Any, target: Any, weight: str = "length"):
+    edge_data = G.get_edge_data(source, target)
     if weight in edge_data:
         edge_length = edge_data[weight]
     else:
         edge_features = next(iter(edge_data.values()))
         assert (
             weight in edge_features
-        ), f"No hay atributo {weight} en los arcos entre los nodos {orig} y {dest}."
+        ), f"No hay atributo {weight} en los arcos entre los nodos {source} y {target}."
         edge_length = edge_features[weight]
     return edge_length
 
 
-def get_edge_length(G, orig, dest):
+def get_edge_length(G: nx.Graph, source: Any, target: Any) -> float:
     assert G.has_edge(
-        orig, dest), f"No hay arco entre los nodos {orig} y {dest}."
-    return get_edge_attribute(G, orig, dest, "length")
+        source, target), f"No hay arco entre los nodos {source} y {target}."
+    return get_edge_attribute(G, source, target, "length")
 
 
-def get_edge_speed(G, orig, dest, avg_speed=25):
-    # Si hay atributo de velocidad en el arco
-    if 'speed_kph' in G.edges[(orig, dest)]:
-        edge_speed = G.edges[(orig, dest)]['speed_kph']
-        return edge_speed
-    return avg_speed
+def get_edge_speed(G: nx.Graph, source, target, avg_speed=25) -> float:
+    # Buscar todos los bordes entre source y target
+    edge_data = G.get_edge_data(source, target)
+    if "speed_kph" in edge_data:
+        return edge_data["speed_kph"]
+    if isinstance(edge_data, dict) and 0 in edge_data:
+        return edge_data[0]['speed_kph'] if 'speed_kph' in edge_data[0] else avg_speed
+    else:
+        return avg_speed
 
 
 def get_edge_cost(
     G,
-    origen: int,
-    destino: int,
-    distribution_name: str = "expectation-lognormal",
-    avg_speed=25,
-):
-    """### Obtener el costo de un arco entre dos nodos
+    source: int,
+    target: int,
+    distribution: str = "expectation-lognormal",
+    avg_speed: float = 25,
+) -> float:
+    """Obtener el costo de un arco entre dos nodos
+
     Parámetros:
+    ----------
         G (Grafo): Grafo con el que se va a trabajar
-        origen (int): Nodo origen
-        destino (int): Nodo destino
-        distribution_name (str): Distribución de probabilidad que se va a utilizar para obtener el costo estocástico. Si no se especifica, el costo es determinístico y se utiliza el valor de la esperanza de la distribución lognormal.
+        source (int): Nodo source (origen)
+        target (int): Nodo target (destino)
+        distribution (str): Distribución de probabilidad que se va a utilizar para obtener el costo estocástico. Si se desea utilizar la esperanza de la distribución, se debe pasar el string "expectation-`distribution`", con un guión separando la palabra "expectation" y el nombre de la distribución. Si se desea obtener una muestra de la distribución, se debe pasar el nombre de la distribución.
+
     Retorno:
-        stochastic_cost (float): Costo del arco entre los nodos origen y destino
+    --------
+        stochastic_cost (float): Costo del arco entre los nodos source y target
     """
-    edge_length = get_edge_length(G, origen, destino)
-    edge_speed = get_edge_speed(G, origen, destino, avg_speed)
+    edge_length = get_edge_length(G, source, target)
+    edge_speed = get_edge_speed(G, source, target, avg_speed)
 
-    # stochastic cost sample from lognormal distribution_name
-    if distribution_name == "expectation-lognormal":
+    # stochastic cost sample from lognormal distribution
+    if "expectation" in distribution:
         # get the expected speed and calculate the expected time given the edge length
-        time = expected_time(edge_length, edge_speed)
+        time = expected_time(edge_length, edge_speed,
+                             distribution.split("-")[1])
 
-    elif distribution_name == "lognormal":
-        # generate a sample of the speed and calculate the time given the edge length
-        time = random_time(edge_length, edge_speed)
     else:
-        raise ValueError(
-            f"La distribución {distribution_name} no está implementada.")
+        # generate a sample of the speed and calculate the time given the edge length
+        time = random_time(edge_length, edge_speed, distribution)
+
     stochastic_cost = time
     return stochastic_cost
 
 
-def get_cumulative_edges_cost(graph, policy, node, dest_node):
+def get_cumulative_edges_cost(graph: nx.Graph, policy: dict, source: Any, target: Any) -> float:
     """
-    Retorna el costo acumulado de comenzar en el nodo 'node' y llegar al nodo 'dest_node' siguiendo la política 'policy'
+    Retorna el costo acumulado de comenzar en el nodo 'source' y llegar al nodo 'target' siguiendo la política 'policy'
+
+    Parámetros:
+    -----------
+        graph (nx.Graph): Grafo con el que se va a trabajar
+        policy (dict): Política de decisión que se va a seguir
+        source (int): Nodo de inicio
+        target (int): Nodo de destino
+
+    Retorno:
+    --------
+        cost (float): Costo acumulado de seguir la política 'policy' desde el nodo 'source' al nodo 'target'
     """
     # Obtener el camino más corto desde el nodo de inicio al nodo de destino dada la política
-    path = [node]
-    while node != dest_node:
-        node = policy[node]
-        path.append(node)
+    path = [source]
+    while source != target:
+        source = policy[source]
+        path.append(source)
     # Calcular el costo acumulado del camino más corto
     cost = 0
     for index in range(len(path) - 1):
@@ -98,7 +117,7 @@ class SSPEnv:
     Entorno de aprendizaje para el problema de encontrar el camino más corto en un grafo
     """
 
-    def __init__(self, graph, start_state, terminal_state):
+    def __init__(self, graph: nx.Graph, start_state: Any, terminal_state: Any, costs_distribution: str = "lognormal"):
         """Constructor de la clase SSPEnv. Inicializa el entorno de aprendizaje con el grafo, el estado inicial y el estado terminal. Notar que al nodo terminal se le agrega un arco recurrente con longitud 0.
 
         Parámetros:
@@ -110,11 +129,11 @@ class SSPEnv:
 
         assert graph is not None, "El graph no puede ser None"
         self.graph = copy.deepcopy(graph)
-        # Añadir un arco de largo 0 en el nodo terminal
+        # Añadir un arco recurrente de largo 0 en el nodo terminal
         if not self.graph.has_edge(terminal_state, terminal_state):
             self.graph.add_edge(terminal_state, terminal_state, length=0)
         else:
-            self.graph[terminal_state][terminal_state]["length"] = 0
+            self.graph[terminal_state][terminal_state][0]["length"] = 0
         self.num_nodos = graph.number_of_nodes()
         assert start_state is not None, "El estado inicial no puede ser None"
         self.__start_state = start_state
@@ -131,49 +150,34 @@ class SSPEnv:
         }  # Diccionario con el número de acciones por estado
         self.__adjacency_dict_of_lists = nx.to_dict_of_lists(self.graph)
         self.__q_table = self.dict_states_actions_zeros()
+        self.__costs_distribution = costs_distribution
 
     def __str__(self):
         return f"EnvShortestPath {self.graph}"
 
     @property
-    def num_states(self):
+    def num_states(self) -> int:
         return self.__num_states
 
     @property
-    def num_actions(self):
+    def num_actions(self) -> Dict[Any, int]:
         return self.__num_actions
 
     @property
-    def start_state(self):
+    def start_state(self) -> Any:
         return self.__start_state
 
     @property
-    def terminal_state(self):
+    def terminal_state(self) -> Any:
         return self.__terminal_state
 
     @property
-    def current_state(self):
+    def current_state(self) -> Any:
         return self.__current_state
 
-    def dict_states_zeros(self):
-        """Retorna un diccionario con los estados con valor 0. Es útil para inicializar la tabla del número de visitas a cada estado, por ejemplo. Tiene la forma {estado: 0, ..., estado: 0}"""
-        return {state: 0 for state, actions in nx.to_dict_of_lists(self.graph).items()}
-
-    def dict_states_actions_zeros(self):
-        """Retorna un diccionario con los estados y acciones con valor 0. Es útil para inicializar la tabla Q, o la tabla de la cantidad de veces que se ha visitado cada par estado-acción. Tiene la forma {estado: {accion: 0, ..., accion: 0}, ..., estado: {accion: 0, ..., accion: 0}}"""
-        G = self.graph
-        return {
-            state: {action: 0 for action in actions}
-            for state, actions in nx.to_dict_of_lists(G).items()
-        }
-
-    def dict_states_actions_random(self):
-        """Retorna un diccionario con los estados y acciones con valor aleatorio. Es útil para inicializar la tabla Q, o la tabla de la cantidad de veces que se ha visitado cada par estado-acción. Tiene la forma {estado: {accion: valor_aleatorio, ..., accion: valor_aleatorio}, ..., estado: {accion: valor_aleatorio, ..., accion: valor_aleatorio}}"""
-        G = self.graph
-        return {
-            state: {action: np.random.random() for action in actions}
-            for state, actions in nx.to_dict_of_lists(G).items()
-        }
+    @property
+    def costs_distribution(self) -> str:
+        return self.__costs_distribution
 
     def reset(self):
         """Reiniciar el entorno de aprendizaje"""
@@ -182,11 +186,15 @@ class SSPEnv:
             f"Entorno reiniciado. Grafo con {self.num_nodos} nodos. Estado inicial: {self.__start_state}. Estado final: {self.__terminal_state}"
         )
 
-    def check_state(self, state):
-        """### Verificar si un estado es válido y/o terminal
+    def check_state(self, state) -> bool:
+        """Verificar si un estado es válido y/o terminal.
+
         Parámetros:
+        ----------
             state (int): Estado a verificar
+
         Retorno:
+        --------
             valid (bool): Indica si el estado es terminal y arroja un error si no es válido
         """
         assert (
@@ -194,12 +202,16 @@ class SSPEnv:
         ), f"El estado {state} no está en el graph"
         return state == self.terminal_state
 
-    def take_action(self, state, action, distribution="expectation-lognormal"):
-        """### Tomar una acción en el entorno de aprendizaje
+    def take_action(self, state, action) -> Tuple[int, float, bool, Dict[str, Dict[str, object]]]:
+        """Tomar una acción en el entorno de aprendizaje
+
         Parámetros:
+        ----------
             state (int): Estado actual
             action (int): Acción a tomar
-        Retorno:
+
+        Retorna:
+        --------
             next_state (int): Siguiente estado
             reward (float): Recompensa (o costo) por tomar la acción
             terminated (bool): Indica si el episodio terminó
@@ -208,13 +220,24 @@ class SSPEnv:
         assert self.graph.has_edge(
             state, action
         ), f"La acción {action} no está en los arcos del nodo {state}"
+        # Obtener el siguiente estado, el costo de la acción y verificar si el estado es terminal
         next_state = action
-        cost = get_edge_cost(self.graph, state, next_state, distribution)
+        cost = get_edge_cost(self.graph, state, next_state,
+                             self.__costs_distribution)
         terminated = self.check_state(next_state)
-        reward = -cost
+        # La recompensa es negativa porque se busca maximizar la recompensa que representa minimizar el costo
+        reward = - cost
         self.__current_state = next_state
         info = {"estado": state, "recompensa": reward, "terminado": terminated}
         return next_state, reward, terminated, info
+
+    def dict_states_actions_zeros(self) -> Dict[str, Dict[str, float]]:
+        """Crear un diccionario con estados y acciones con valores 0"""
+        return dict_states_actions_zeros(self.graph)
+
+    def dict_states_zeros(self) -> Dict[str, float]:
+        """Crear un diccionario con estados con valores 0"""
+        return dict_states_zeros(self.graph)
 
     def calculate_optimal_qtable(self):
         """### Calcular la tabla Q óptima"""
